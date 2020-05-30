9a16908089714ffb7bd4c81022c01df573d2ccbc
Corinna Jaschek <corinna.jaschek@gmail.com>
2020-05-30 19:33:48 +0200

    assign player to random team, set sprite accordingly

--- player/player.gd
+++ player/player.gd
@@ -6,7 +6,7 @@ var color: Color setget set_color
 var selected_building
 const speed = 200
 var inventary = {}
-var good_team = true
+var good_team setget set_team
 
 func _ready():
 	rset_config("position", MultiplayerAPI.RPC_MODE_REMOTESYNC)
@@ -17,12 +17,13 @@ func _ready():
 	# pick our color, even though this will be called on all clients, everyone
 	# else's random picks will be overriden by the first sync_state from the master
 	set_color(Color.from_hsv(randf(), 1, 1))
+	set_team(randf() >= 0.5)
 	$Camera2D.current = is_network_master()
 	
 
 func get_sync_state():
 	# place all synced properties in here
-	var properties = ['color']
+	var properties = ['color', 'good_team']
 	
 	var state = {}
 	for p in properties:
@@ -61,6 +62,10 @@ func _process(dt):
 func set_color(_color: Color):
 	color = _color
 	$sprite.modulate = color
+
+func set_team(team):
+	good_team = team
+	$sprite.texture = load("res://player/sloth.png") if good_team else load("res://player/koala.png")
 
 remotesync func spawn_projectile(position, direction, name):
 	var projectile = preload("res://examples/physics_projectile/physics_projectile.tscn").instance()
