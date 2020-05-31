5b5d3af0076dddd627d4d7934c4902ba97d86132
Tom Beckmann <tomjonabc@gmail.com>
2020-05-31 13:56:29 +0200

    print host

--- game/game.gd
+++ game/game.gd
@@ -55,6 +55,7 @@ func _ready():
 	var host = 'g.tmbe.me' if '-t' in OS.get_cmdline_args() else ip
 	
 	if is_client:
+		print('Connecting to ' + str(host) + ':' + str(port))
 		peer.create_client(host, port)
 		if get_tree().connect("server_disconnected", self, "client_note_disconnected") != OK:
 			print("An eror occured while trying to connect the server disconnected signal")
