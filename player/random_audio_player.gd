7313b185be73c0cd4559dc3c7590fa653715e7a4
Sebastian Schulz <sebastian.schulz@hpi.de>
2020-05-31 18:38:29 +0200

    limit audio distance

--- player/random_audio_player.gd
+++ player/random_audio_player.gd
@@ -1,8 +1,23 @@
-extends Node
+extends Node2D
 
 onready var sounds = get_children()
 var rng = RandomNumberGenerator.new()
 
 remotesync func play_random():
+	var players = get_tree().get_nodes_in_group("players")
+	var success = false
+	var cam_pos
+	for player in players:
+		if player.is_network_master():
+			success = true
+			cam_pos = player.global_position
+			
+	if not success:
+		print(":(")
+		return
+	
+	#var cam_pos = get_viewport().get_camera().get_global_pos()
+	if global_position.distance_to(cam_pos) > 1000:
+		return
 	var index = rng.randi_range(0, sounds.size() - 1)
 	sounds[index].play()
