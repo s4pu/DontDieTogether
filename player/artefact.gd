6caba6a6e56ac0dc9f7d3988a6da058b8149f911
Tom Beckmann <tomjonabc@gmail.com>
2020-05-31 19:02:04 +0200

    add healthbar to arterfact

--- player/artefact.gd
+++ player/artefact.gd
@@ -2,22 +2,24 @@ extends StaticBody2D
 
 signal game_over
 
-var hp = 200
+const MAX_HP = 200
+var hp = MAX_HP setget set_hitpoints
 var good_team
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func set_hitpoints(num):
+	hp = num
+	$Health.value = num / float(MAX_HP)
+
 func destroy():
 	queue_free()
 	emit_signal("game_over")
 
 remotesync func take_damage(damage):
-	hp -= damage
+	set_hitpoints(hp - damage)
+	
 	if (hp <= 0):
 		destroy()
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
